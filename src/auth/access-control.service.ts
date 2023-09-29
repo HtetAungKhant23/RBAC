06c8684c1d732e01695cfd54764eb94be4fc8138
htetaungkhant <htetaung23.ha@gmail.com>
2023-09-29 17:32:08 +0630

    add: access-control and role guard

---
+++ src/auth/access-control.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import { Role } from "./enums/role.enum";
+
+interface IsAuthorizedParams {
+  currentRole: Role;
+  requiredRole: Role;
+}
+
+@Injectable()
+export class AccessControlService {
+  private socialOrders: Array<Map<string, number>> = [];
+  private priority: number = 1;
+
+  constructor() {
+    this.buildRoles([Role.User, Role.Moderator, Role.SuperAdmin]);
+    this.buildRoles([Role.Admin, Role.SuperAdmin]);
+    console.log(this.priority, " => priority");
+  }
+
+  private buildRoles(roles: Role[]) {
+    const order: Map<string, number> = new Map();
+    roles.forEach(role => {
+      order.set(role, this.priority);
+      this.priority++;
+    });
+    this.socialOrders.push(order);
+  }
+
+  public isAuthorized({ currentRole, requiredRole }: IsAuthorizedParams): boolean {
+    for (let order of this.socialOrders) {
+      const priority = order.get(currentRole);
+      const requiredPriority = order.get(requiredRole);
+      if (priority && requiredPriority && priority >= requiredPriority) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -2,11 +2,29 @@ import { CanActivate, ExecutionContext } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { Role } from "../enums/role.enum";
 import { Role_Key } from "../decorators/role.decorator";
+import { Observable } from "rxjs";
+import { AccessControlService } from "../access-control.service";
+
+export type currentUserDto = {
+  id: string;
+  role: Role;
+};
 
 export class RoleGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector, private accessControlService: AccessControlService) {}
 
-  canActivate(context: ExecutionContext): boolean {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(Role_Key, [context.getClass(), context.getClass()]);
+    const request = context.switchToHttp().getRequest();
+    const currentUser = request["user"] as currentUserDto;
+
+    for (let role of requiredRoles) {
+      const result = this.accessControlService.isAuthorized({ currentRole: currentUser.role, requiredRole: role });
+      if (result) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
