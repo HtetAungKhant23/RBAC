fb405cf018726c27e613dad3891bad8df5194aa8
htetaungkhant <htetaung23.ha@gmail.com>
2023-09-29 16:27:21 +0630

    add: register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { CreateAuthDto } from "./dto/create-auth.dto";
+import { RegisterDto } from "./dto/create-auth.dto";
 import { ApiBody, ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("auth")
@@ -10,8 +10,8 @@ export class AuthController {
 
   @Post()
   @ApiOperation({ summary: "Register" })
-  @ApiBody({ type: CreateAuthDto })
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @ApiBody({ type: RegisterDto })
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,68 @@
-import { Injectable } from "@nestjs/common";
-import { CreateAuthDto } from "./dto/create-auth.dto";
+import { HttpException, Injectable } from "@nestjs/common";
+import { RegisterDto } from "./dto/create-auth.dto";
 import { PrismaService } from "src/prisma.service";
+import { JwtService } from "@nestjs/jwt";
+import { Responser } from "src/libs/responser";
 
+type ReturnToken = { accessToken: string; refreshToken: string };
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
-  async create(createAuthDto: CreateAuthDto) {
-    return "This action adds a new auth";
+  async register(dto: RegisterDto) {
+    try {
+      const existingUser = await this.prisma.user.findUnique({
+        where: {
+          phone: dto.phone,
+        },
+      });
+      if (existingUser) throw new Error("User Already Exist!");
+      const newUser = await this.prisma.user.create({
+        data: {
+          phone: dto.phone,
+          password: dto.password,
+          profile: {
+            create: {
+              name: dto.name,
+              role: dto.role,
+            },
+          },
+        },
+        include: {
+          profile: true,
+        },
+      });
+
+      const token = await this.generateToken(newUser.id, newUser.profile.role);
+
+      return Responser({
+        statusCode: 201,
+        message: "Registration Success",
+        devMessage: "register-success",
+        body: { accessToken: token.accessToken },
+      });
+    } catch (err) {
+      throw new HttpException(
+        {
+          message: err.message || "Failed to register!",
+          devMessage: err.message || "",
+        },
+        401,
+      );
+    }
   }
+
+  private generateToken = async (id: string, role: string): Promise<ReturnToken> => {
+    const payload = { id, role };
+    const accessToken = await this.jwtService.signAsync(payload, {
+      secret: process.env.JWT_ACCESS_KEY,
+      expiresIn: "1d",
+    });
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: process.env.JWT_REFRESH_KEY,
+      expiresIn: "7d",
+    });
+
+    return { accessToken, refreshToken };
+  };
 }
