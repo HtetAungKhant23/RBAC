896c4ba280f361b58a6dd9583df248768898bd1a
htetaungkhant <htetaung23.ha@gmail.com>
2023-10-04 10:02:26 +0630

    add: global decorator in app module and export prisma service and jwt service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { Global, Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { PrismaService } from "./prisma.service";
+import { JwtService } from "@nestjs/jwt";
 
+@Global()
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, JwtService],
+  exports: [PrismaService, JwtService],
 })
 export class AppModule {}
